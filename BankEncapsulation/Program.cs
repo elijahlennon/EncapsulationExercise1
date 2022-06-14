f98afc969ea06c50bb6f0fb4ac5526f03c904685
Elijah Lennon <elijahlennon1@gmail.com>
2022-06-13 23:09:26 -0500

    Completed Exercise

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var elijahsAccount = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            elijahsAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Your balance is now ${elijahsAccount.GetBalance()}.");
+        }
+    }
+}
